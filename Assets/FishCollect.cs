cd8e1a822fe03149d81e183873b8eded583cd56f
maximpoltar-droid <maxim.poltar@gmail.com>
2026-03-07 11:05:35 +0100

    Commit

--- Assets/FishCollect.cs
+++ Assets/FishCollect.cs
@@ -2,13 +2,21 @@ using UnityEngine; // Основная библиотека Unity
 using System; // Для Array, String и т.д.
 using System.Collections; // Для корутин (не обязательно для тегов)
 using System.Collections.Generic; // Для List, Dictionary
+using TMPro;
 
 
 public class FishCollect : MonoBehaviour
 
 
 {
+    public TextMeshProUGUI eattenFishText;
     public int eattenFish = 0;
+    void Start()
+    {
+        eattenFish = 0;
+        eattenFishText.text = "EATEN FISH = " + eattenFish.ToString();     
+    }
+
     void Update()
     {
 
@@ -19,6 +27,8 @@ public class FishCollect : MonoBehaviour
         {
             Debug.Log("Seal ate fish");
             eattenFish += 1;
+            eattenFishText.text ="EATEN FISH = "+ eattenFish.ToString();
+            Destroy(collision.gameObject);
         }
     }
 }
